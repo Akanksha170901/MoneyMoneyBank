22c47068fa7b0f01cfcc806ff8a6740225f4800c
atish <atish@LAPTOP-89EBFAAT.mshome.net>
2022-03-09 15:00:43 +0530

    Projecr commited

---
+++ MoneyMoneyBank/src/com/tns/application/MMBankFactory.java
@@ -0,0 +1,20 @@
+package com.tns.application;
+
+import com.tns.framework.BankFactory;
+import com.tns.framework.CurrentAcc;
+import com.tns.framework.SavingAcc;
+
+public class MMBankFactory extends BankFactory {
+	
+	public SavingAcc getNewSavingAccount(int accNo, String accNm, float accBal, boolean isSalary) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CurrentAcc getNewCurrentAccount(int accNo, String accNm, float accBal, float creditLimit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
